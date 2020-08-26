912eafb931024af61e2d07bba539abfca5172136
Rafael Pivato <rpivato@gmail.com>
2020-08-25 22:05:41 -0300

    Field value stored with descriptor

--- albus/alpha.py
+++ albus/alpha.py
@@ -1,53 +1,66 @@
 # albus.model
-from abc import ABC, abstractmethod
-from typing import Any
-
-from albus.exceptions import AlbusError
 
 
-class Data(ABC):
+class BaseModel:
 
-    @abstractmethod
-    def _store_value(self, value: Any, key: str = None):
-        raise NotImplementedError()
+    def __init_subclass__(cls):
+        super().__init_subclass__()
+        cls.__fields = {}
 
-    @abstractmethod
-    def _load_value(self, key: str = None):
-        raise NotImplementedError()
+    @classmethod
+    def register_field(cls, field):
+        cls.__fields[field.name] = field
+
+    @classmethod
+    def enumerate_fields(cls):
+        for name, field in cls.__fields.items():
+            yield name, field
 
 
-class Model(Data):
+class Model(BaseModel):
 
-    def __init__(self):
+    def to_json(self):
+        result = {}
+        for name, field, value in self.enumerate_field_values():
+            result[name] = field.to_json(value)
+        return result
+
+    def enumerate_fields_values(self):
+        for name, field in type(self).enumerate_fields():
+            value = getattr(self, name)
+            yield name, field, value
+
+
+class BaseField:
+
+    def __init__(self, default=None):
         self.__data = {}
-
-    def _store_value(self, value: Any, key: str = None):
-        self.__data[key] = value
-
-    def _load_value(self, key: str = None):
-        return self.__data[key]
-
-
-class FieldDescriptor(Data):
+        self.default = default
 
     def __set_name__(self, owner, name):
-        if not issubclass(owner, Model):
-            raise AlbusError(
-                "Bad container",
-                detail="Fields can only be bound to models.",
-            )
         self.owner = owner
         self.name = name
+        owner.register_field(self)
 
-    def __get__(self, obj, cls=None):
+    def __get__(self, obj, owner=None):
         if obj is None:
             return self
-        return self.owner._load_value(key=self.name)
+        return self.__data.get(obj, self.default)
 
     def __set__(self, obj, value):
-        obj._store_value(value, key=self.name)
+        self.__data[obj] = value
 
 
-class Field(FieldDescriptor):
+class Field(BaseField):
+
+    pass
+
+
+class IntegerField(Field):
+
+    pass
+
+
+class StringField(Field):
 
     pass
